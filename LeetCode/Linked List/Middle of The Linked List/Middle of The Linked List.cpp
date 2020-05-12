049fefc699c01c0c79b47fa44ad7748c0d13efda
Delight Joshua <53788573+DelightJoshua@users.noreply.github.com>
2020-05-12 15:54:52 +0530

    Update Middle of The Linked List.cpp

--- LeetCode/Linked List/Middle of The Linked List/Middle of The Linked List.cpp
+++ LeetCode/Linked List/Middle of The Linked List/Middle of The Linked List.cpp
@@ -1,9 +1,9 @@
- ListNode* middleNode(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* temp = head;
-        while(temp != nullptr && temp -> next != nullptr){
-            curr = curr -> next;
-            temp = temp -> next -> next;
-        }
-        return curr;
-    }
+ListNode* middleNode(ListNode* head) {
+       ListNode* curr = head;
+       ListNode* temp = head;
+       while(temp != nullptr && temp -> next != nullptr){
+           curr = curr -> next;
+           temp = temp -> next -> next;
+       }
+       return curr;
+}
