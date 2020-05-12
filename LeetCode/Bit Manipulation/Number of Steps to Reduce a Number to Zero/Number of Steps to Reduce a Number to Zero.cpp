f105d4c7db907fe949f8ff9bdecdb226f1623eba
Delight Joshua <53788573+DelightJoshua@users.noreply.github.com>
2020-05-12 15:53:08 +0530

    Update Number of Steps to Reduce a Number to Zero.cpp

--- LeetCode/Bit Manipulation/Number of Steps to Reduce a Number to Zero/Number of Steps to Reduce a Number to Zero.cpp
+++ LeetCode/Bit Manipulation/Number of Steps to Reduce a Number to Zero/Number of Steps to Reduce a Number to Zero.cpp
@@ -1,13 +1,13 @@
-    int numberOfSteps (int num) {
-        int count = 0;
-        while(num != 0){
-            if(num % 2 == 0){
-                num = num/2;
-            }
-            else{
-                num--;
-            }
-            count++;
-      }
-        return count;
-    }
+int numberOfSteps (int num) {
+    int count = 0;
+    while(num != 0){
+        if(num % 2 == 0){
+            num = num/2;
+        }
+        else{
+            num--;
+        }
+        count++;
+  }
+    return count;
+}
